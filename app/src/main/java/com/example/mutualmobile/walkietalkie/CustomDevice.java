684a968bda02a3745cc93aa8db89b65eb4993167
Vartul Tripathi <vartult2@gmail.com>
2020-02-18 17:46:00 +0530

    working code 1-1

---
+++ app/src/main/java/com/example/mutualmobile/walkietalkie/CustomDevice.java
@@ -0,0 +1,17 @@
+package com.example.mutualmobile.walkietalkie;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.view.View;
+
+/**
+ * Created by Vivek Patel on 2019-11-18.
+ */
+class CustomDevice{
+  int id;
+  String deviceName;
+  WifiP2pDevice device;
+  View icon_view;
+  CustomDevice(){
+
+  }
+}
